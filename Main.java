b6d23a880f4ad44576b3a8582a77380677e53af6
Itax-Carmel <140900858+Itax-Carmel@users.noreply.github.com>
2023-07-29 14:42:53 +0000

    Ajout des fonctions, une qui calcul la factorielle d'un nombre saisi par le user, et l'autre qui affiche dix entiers dont le nombre de depart est saisi par le user

--- Main.java
+++ Main.java
@@ -4,39 +4,27 @@ public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     //Exercices sur les boucles avec JAVA
-    /*1. Ecrire un programme qui demande un nombre de départ, et qui ensuite affiche les dix nombres suivants. Par exemple, si l'utilisateur entre le nombre 17, le programme affichera les nombres de 18 à 27.*/
-    public static void dixEntierSuivant (){
-      System.out.println("Veuillez saisir le nombre de depart: ");
-      int numberUser = scanner.nextInt();
-      for(int i = numberUser + 1; i <= numberUser + 10; i++){
-       System.out.println(i);
-      }
-    }
-    /*
-    System.out.println("Veuillez saisir un nombre pour calculer sa factorielle: ");
-    int numberUser = scanner.nextInt();
-    int factorielle = 1;
-    for(int i = 1; i <= 10; i++){
-      factorielle = numberUser * i;
-      System.out.println(numberUser + " * " + i + " = " + factorielle);
-    }
-    //System.out.println(numberUser + "! = " + factorielle);
-    /*int add = 0;
-    for(int i = 1; i <= numberUser; i++){
-      add = add + i;
-    }
-    System.out.println("L'add des entiers jusqu'à " + numberUser + " = " + add);
-    /*
+    dixEntierSuivant(scanner);
+    factorielle(scanner);
+    
+    scanner.close();  
+  }
+  /*1. Ecrire un programme qui demande un nombre de départ, et qui ensuite affiche les dix nombres suivants. Par exemple, si l'utilisateur entre le nombre 17, le programme affichera les nombres de 18 à 27.*/
+  static void dixEntierSuivant (Scanner scanner){
     System.out.println("Veuillez saisir le nombre de depart: ");
     int numberUser = scanner.nextInt();
     for(int i = numberUser + 1; i <= numberUser + 10; i++){
       System.out.println(i);
     }
-    /*
-    System.out.println("Veuillez saisir le num2: ");
-    int num2 = scanner.nextInt();
-    int result = num1 + num2;
-    System.out.println("Le résultat de " + num1 + " + " + num2 + " = " + result);*/
-    scanner.close();
+  }
+  /*2. Ecrire un programme qui calcule le factoriel d’un entier saisi au clavier.*/
+  static void factorielle (Scanner scanner){
+    System.out.println("Veuillez saisir un nombre pour trouver sa factorielle: ");
+    int numberUser = scanner.nextInt();
+    int factorielle = 1;
+    for(int i = 1; i <= numberUser; i++){
+      factorielle = factorielle * i;
+    }
+    System.out.println(numberUser + "! = " + factorielle);
   }
 }
